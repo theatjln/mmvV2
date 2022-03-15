8972441e44b46cef016ad9df1f918c7fc50a012f
theatjln <janicaliz.dev@gmail.com>
2022-03-15 16:44:35 +0800

    added future: { webpack5: true } in next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@
 
 const path = require("path");
 module.exports = {
+  future: { webpack5: true },
   reactStrictMode: true, //equivalent to 'use strict'
   trailingSlash: true, //whenever a user goes to any particular url, its gonna automatically add a trailing slash if it doesn exist, good for SEO
   webpackDevMiddleware: (config) => {
