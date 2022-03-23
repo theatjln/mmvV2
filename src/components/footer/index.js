254eec95c085c27b39be5071b032b464444bbc3f
theatjln <janicaliz.dev@gmail.com>
2022-03-23 13:37:52 +0800

    changed markus markus to mark markus

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
           <span className="ml-3 text-xl font-display">{name}</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2022 Markus Markus Viajero —
+          © 2022 Mark Markus Viajero —
           <a
             href={`https://instagram.com/${instagramUsername}`}
             className="text-gray-600 ml-1 font-display text-lg"
